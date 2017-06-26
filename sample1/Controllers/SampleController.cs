a7cfef1b8d574de8f68a1c2a109ecafad8f02e21
Liang Yi <liangyi2012@gmail.com>
2017-06-26 22:44:32 +0800

    init

---
+++ sample1/Controllers/SampleController.cs
@@ -0,0 +1,22 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace sample1.Controllers
+{
+    public class SampleController : Controller
+    {
+        public IActionResult Sample1()
+        {
+            return View();
+        }
+
+        public IActionResult Sample2()
+        {
+            return View();
+        }
+    }
+}
